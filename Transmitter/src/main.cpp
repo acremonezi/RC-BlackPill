f8f9199f33a4ab2bc7214e15999d1d9493981c85
Alcides <acremonezi@gmail.com>
2021-12-24 00:28:26 -0300

    start

--- Transmitter/src/main.cpp
+++ Transmitter/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(100);                       
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); 
 }
